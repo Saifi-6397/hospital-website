6869fa88091f56be2d5f32dee0340a3ebb4cadb5
Saifi-6397 <khaleelahmad3886@gmail.com>
2021-12-16 13:05:02 +0530

    update

--- js/script.js
+++ js/script.js
@@ -29,13 +29,15 @@ $(function() {
         nav: false,
         responsive: {
             0: {
-                items: 1
+                items: 1,
             },
             600: {
-                items: 2
+                items: 2,
+                margin: 30,
             },
             900: {
-                items: 3
+                items: 3,
+                margin: 60,
             },
             1400: {
                 items: 5
@@ -49,3 +51,49 @@ $(function() {
         delay: 400,
     });
 })
+
+//form validation
+
+var user_name = document.getElementById("name");
+var user_phone = document.getElementById("phone");
+var user_email = document.getElementById("email");
+var user_dropdown = document.getElementById("dropdown");
+var return_val = 1;
+
+function Validation() {
+    if (user_name.value == "") {
+        document.getElementById("name_error").innerHTML = "Please Enter Your Name";
+        return_val = 0;
+    } else {
+        document.getElementById("name_error").innerHTML = "";
+        return_val = 1;
+    }
+
+    if (user_phone.value == "") {
+        document.getElementById("phone_error").innerHTML = "Please Enter Your Phone No";
+        return_val = 0;
+    } else {
+        document.getElementById("phone_error").innerHTML = "";
+        return_val = 1;
+    }
+
+    if (user_email.value == "") {
+        document.getElementById("email_error").innerHTML = "Please Enter Your Email";
+        return_val = 0;
+    } else {
+        document.getElementById("email_error").innerHTML = "";
+        return_val = 1;
+    }
+    if (user_dropdown.value == "") {
+        document.getElementById("dropdown_error").innerHTML = "Please Enter Your Time";
+        return_val = 0;
+    } else {
+        document.getElementById("dropdown_error").innerHTML = "";
+        return_val = 1;
+    }
+    if (return_val) {
+        return true;
+    } else {
+        return false;
+    }
+}
